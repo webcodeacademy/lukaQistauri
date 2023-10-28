739d4b65c5075d84398543bd1c8eedf2b784e8ec
webcodeacademy <webcodeacademyinfo@gmail.com>
2023-10-28 18:33:27 +0400

    lukaQistauri

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    dotsEach:true,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
